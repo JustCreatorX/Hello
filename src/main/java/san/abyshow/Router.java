00be82a027b99e8f634a35792fb1b48a26f9e0f1
JustCreatorX <oleksii.abyshov@gmail.com>
2018-05-29 23:07:59 +0200

    master

---
+++ src/main/java/san/abyshow/Router.java
@@ -0,0 +1,15 @@
+package san.abyshow;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Router {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "index.html";
+    }
+}
+
---
+++ src/test/java/san/abyshow/AbyshowApplicationTests.java
@@ -0,0 +1,16 @@
+package san.abyshow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbyshowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
